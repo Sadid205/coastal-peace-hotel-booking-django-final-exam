fe06c89a7c4efbef3da526cb2f0ce5d26efda5b7
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2024-07-11 01:42:22 +0600

    changed logout method

--- guest/views.py
+++ guest/views.py
@@ -80,7 +80,7 @@ class UserLoginApiView(APIView):
 
 class UserLogoutApiView(APIView):
     permission_classes = [IsAuthenticated]
-    def get(self,request):
+    def post(self,request):
         request.user.auth_token.delete()
         logout(request)
         return Response({"Success":"Logout Success"})
