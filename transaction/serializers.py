a90e0e1cf0686d94206e811f2295ad646f45287e
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2024-07-10 08:35:19 +0600

    transaction serializer problem fixed

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Transaction
+from guest.models import Guest
+from booking.models import Booking
 
 class TransactionSerializer(serializers.ModelSerializer):
-    guest = serializers.StringRelatedField(many=True)
-    booking = serializers.StringRelatedField(many=True)
+    guest = serializers.PrimaryKeyRelatedField(queryset=Guest.objects.all(),many=False)
+    booking = serializers.PrimaryKeyRelatedField(queryset=Booking.objects.all(),many=False)
     class Meta:
         model = Transaction
         fields = "__all__"
